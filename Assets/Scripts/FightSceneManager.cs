a7f6b4ebbf4f0c57d50c66080d82343c14980bf7
Ben Costrell <bencostrell@gmail.com>
2017-02-05 14:47:56 -0500

    joystick input, basic attack animation

--- Assets/Scripts/FightSceneManager.cs
+++ Assets/Scripts/FightSceneManager.cs
@@ -10,6 +10,8 @@ public class FightSceneManager : MonoBehaviour {
 	public GameObject playerPrefab;
 	public Vector3 player1Spawn;
 	public Vector3 player2Spawn;
+	public RuntimeAnimatorController player1Anim;
+	public RuntimeAnimatorController player2Anim;
 
 	// Use this for initialization
 	void Start () {
@@ -25,9 +27,12 @@ public class FightSceneManager : MonoBehaviour {
 		player1 = Instantiate (playerPrefab, player1Spawn, Quaternion.identity) as GameObject;
 		player1.GetComponent<SpriteRenderer> ().sprite = player1Sprite;
 		player1.GetComponent<PlayerController> ().playerNum = 1;
+		player1.GetComponent<Animator> ().runtimeAnimatorController = player1Anim;
 
 		player2 = Instantiate (playerPrefab, player2Spawn, Quaternion.identity) as GameObject;
 		player2.GetComponent<SpriteRenderer> ().sprite = player2Sprite;
 		player2.GetComponent<PlayerController> ().playerNum = 2;
+		player2.GetComponent<Animator> ().runtimeAnimatorController = player2Anim;
+
 	}
 }
