a6d2dc96b32c470fc276c33464bd8fbff2be0e4c
DavidFrisch <dmf463@nyu.edu>
2017-02-05 14:41:17 -0500

    StartedPickupScript

---
+++ Assets/Scripts/PlayerPickupScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickupScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collider other)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
