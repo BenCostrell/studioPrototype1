7addfd1b5d39b0d9fcf7e1659427b77fbc1c6337
Ben Costrell <bencostrell@gmail.com>
2017-02-05 11:58:34 -0500

    player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+	public int playerNum;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+		Vector2 direction = new Vector2 (Input.GetAxisRaw ("Horizontal_P" + playerNum), Input.GetAxisRaw ("Vertical_P" + playerNum));
+
+		direction = direction.normalized;
+
+		rb.velocity = speed * direction;
+	}
+}
